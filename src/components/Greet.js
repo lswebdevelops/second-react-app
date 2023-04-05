01e63cd8d85ac7e0407e86acb5a326da34d83d6e
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-04-05 20:48:26 -0300

    adding variable name for testig

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+const name = "davi"
 /* 
 function Greet(){
      return <h1>Hello Davi</h1>
@@ -7,6 +7,6 @@ function Greet(){
 
  using ES6
  */
-const  Greet = ()=> <h1>Hello Davi</h1>
+const  Greet = ()=> <h1>Hello {name}</h1>
 
 export default Greet;
